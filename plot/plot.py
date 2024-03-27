87af7d85611992e0974d718ad54229f210d543f3
alya <alyaggomaa@gmail.com>
2024-03-27 23:11:55 +0200

    plot.py: don't do any calculations, just plot the given metrics per threshold.

--- plot/plot.py
+++ plot/plot.py
@@ -4,31 +4,47 @@ from typing import Dict, List
 
 
 class Plot:
-
     def line(self,
-        metrics: Dict[int, Dict[str, Dict[str, float]]],
-        metrics_to_plot: List[str]
-        ):
-    
-        # Extract x-axis labels
-        x = list(metrics.keys())
+            metrics_per_threshold: Dict[int, Dict[str, float]],
+            title: str
+            ):
+        """
+        Each call to this function results in one graph/plot , each metric in
+        metrics_to_plot will result in one line in this graph.
+        X-axis is determined by the metrics.keys()
+        Y-axis is determined by the metrics.values().values()
+        
+        :param metrics_per_threshold: a dict in the following format:
+        {
+            1: { 'TP': 4, 'TN': 0, 'FP': 10, 'FN':12}
+            2: { 'TP': 4, 'TN': 0, 'FP': 10, 'FN':12} ...
+        }
+        
+        :param title: Title of the plot
+        """
+        
+        # Extract x-axis labels. put all thresholds on the x-axis.
+        x_axis = list(metrics_per_threshold.keys())
+        # keys(aka metrics to plot) should be identical in all thresholds
+        metrics_to_plot: List[str] = list(
+            metrics_per_threshold.values()
+            )[0].keys()
+
         for metric in metrics_to_plot:
-            y = []
             # Plot each metric as a separate line
-            for threshold, experiments in metrics.items():
-                _sum = 0
-                for exp_name, exp_values in experiments.items():
-                    _sum += exp_values[metric]
-                
-                y.append(_sum)
-            plt.plot(x, y, label=metric)
-    
-        plt.xlabel('thresholds')
-        plt.ylabel("metrics")
-        plt.title('Metrics Over Thresholds')
+            y_axis = []
+            for threshold in x_axis:
+                # get the metric value in this threshold
+                y_axis.append(metrics_per_threshold[threshold][metric])
+            plt.plot(x_axis, y_axis, label=metric)
+            
+
+        plt.title(title)
+        plt.xlabel('Thresholds')
+        plt.ylabel("Metrics")
         plt.legend()
         plt.show()
-    
+
     
     def scatter(self,
         metrics: Dict[int, Dict[str, Dict[str, float]]],
